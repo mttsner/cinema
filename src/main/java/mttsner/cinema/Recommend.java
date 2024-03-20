3653e5f4c31c97b19bec918929fb3cc585b50005
mttsner <42277794+mttsner@users.noreply.github.com>
2024-03-20 16:21:11 +0200

    Add recommendation algorithm

---
+++ src/main/java/mttsner/cinema/Recommend.java
@@ -0,0 +1,60 @@
+package mttsner.cinema;
+
+import mttsner.cinema.movies.AgeRating;
+import mttsner.cinema.movies.Genre;
+import mttsner.cinema.movies.Language;
+import mttsner.cinema.movies.Movie;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Recommend {
+    private final HashMap<Genre, Integer> genreWeights;
+    private final HashMap<Language, Integer> languageWeights;
+    private final HashMap<AgeRating, Integer> ageRatingWeights;
+    private final HashMap<Integer, Integer> cache;
+
+    public Recommend(List<Movie> movies) {
+        genreWeights = new HashMap<>();
+        languageWeights = new HashMap<>();
+        ageRatingWeights = new HashMap<>();
+        cache = new HashMap<>();
+        // Update weights based on movie history
+        for (Movie movie: movies) {
+            updateWeights(movie);
+        }
+    }
+
+    private void updateWeights(Movie movie) {
+        // Update genre weights
+        for (Genre genre: movie.getGenres()) {
+            int gen = genreWeights.getOrDefault(genre, 0);
+            genreWeights.put(genre, gen + 1);
+        }
+        // Update language weight
+        int lang = languageWeights.getOrDefault(movie.getLanguage(), 0);
+        languageWeights.put(movie.getLanguage(), lang + 1);
+        // Update age rating weight
+        int age = ageRatingWeights.getOrDefault(movie.getAgeRating(), 0);
+        ageRatingWeights.put(movie.getAgeRating(), age + 1);
+    }
+
+    public int getScore(Movie movie) {
+        // Memoize the score
+        if (cache.containsKey(movie.getMovieId())) {
+            return cache.get(movie.getMovieId());
+        }
+        int score = 0;
+        // Increase score based on genre weights
+        for (Genre genre : movie.getGenres()) {
+            score += this.genreWeights.getOrDefault(genre, 0);
+        }
+        // Increase score based on language weights
+        score += this.languageWeights.getOrDefault(movie.getLanguage(), 0);
+        // Increase score based on age rating weights
+        score += this.ageRatingWeights.getOrDefault(movie.getAgeRating(), 0);
+        // Save score to cache
+        cache.put(movie.getMovieId(), score);
+        return score;
+    }
+}
--- src/main/java/mttsner/cinema/ScheduleController.java
+++ src/main/java/mttsner/cinema/ScheduleController.java
@@ -1,27 +1,31 @@
 package mttsner.cinema;
 
-import mttsner.cinema.movies.AgeRating;
-import mttsner.cinema.movies.Genre;
-import mttsner.cinema.movies.Language;
+import jakarta.transaction.Transactional;
+import mttsner.cinema.movies.*;
+import mttsner.cinema.schedule.Schedule;
 import mttsner.cinema.schedule.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
 @Controller
-public class ScheduleController
-{
+public class ScheduleController {
     @Autowired
     private ScheduleRepository scheduleRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     @GetMapping("/schedule")
     public String schedule(Model model) {
@@ -41,14 +45,30 @@ public class ScheduleController
         return "schedule/index";
     }
 
+    @Transactional
     @PostMapping("/filter")
     public String filter(@RequestParam(required = false) List<Genre> genres,
                          @RequestParam(required = false) List<AgeRating> ages,
                          @RequestParam(required = false) List<Language> languages,
                          @RequestParam(required = false) List<String> times,
+                         @RequestParam List<Integer> history,
                          @RequestParam LocalDate date,
                          Model model) {
-        model.addAttribute("schedule", scheduleRepository.findByFilters(date, genres, ages, languages, times));
+        List<Schedule> schedules = scheduleRepository.findByFilters(date, genres, ages, languages, times);
+        // If no history is provided, don't run the recommendation algorithm
+        if (history.isEmpty()) {
+            model.addAttribute("schedule", schedules);
+            return "schedule/index::items";
+        }
+        List<Movie> movies = movieRepository.findAllById(history);
+        // Recommend based on history of watched movies
+        Recommend recommend = new Recommend(movies);
+        // Custom function used for sorting
+        Comparator<Schedule> comparator = Comparator.comparing(schedule -> recommend.getScore(schedule.getMovieId()));
+        // Sort schedule items based on recommendation score
+        schedules.sort(comparator);
+        // Render the schedule items
+        model.addAttribute("schedule", schedules);
         return "schedule/index::items";
     }
 }
